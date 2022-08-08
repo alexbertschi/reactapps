0b4616428f2e4ee24e55bd9ecf6db3fa923766d8
alexbertschi <alex.bertschi@gmail.com>
2022-08-08 16:37:40 +0200

    Simple connection to ganache

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Web3 from 'web3'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Do edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentWillMount() {
+    this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:7545")
+    const accounts = await web3.eth.getAccounts()
+    const detail = await web3.eth.currentProvider
+    //getBalance(accounts[0])
+    this.setState({ account: accounts, details: detail.networkVersion})
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = { account: '', details: 'test' }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Hello, World!</h1>
+        <span>My account: {this.state.account} </span><br/><span> Network Version: {this.state.details}</span>
+      </div>
+      
+    );
+  }
 }
 
 export default App;
