e0c685a0322bdc3d5893ec0545cc13db34eb52af
alexbertschi <alex.bertschi@gmail.com>
2022-08-10 13:09:52 +0200

    Showing the Token Balance on the Screen (full dynamic read access)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css'
 import abiArray from './Liontoken.json'
 
 class App extends Component {
-  componentWillMount() {
+  componentDidMount() {
     this.loadBlockchainData()
   }
 
@@ -12,48 +12,42 @@ class App extends Component {
     const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
     const accounts = await web3.eth.getAccounts();
     const detail = await web3.eth.getBalance(accounts[0]);
-    this.setState({ account: accounts, details: detail});
-
-    const contractAddress = '0x07b729491574F0b0b4f20c2BdDA8B306B5dC58FD'
-
-    var MyContract = new web3.eth.Contract(abiArray.abi, contractAddress);
-
-    console.log(MyContract);
-    const result = await MyContract.methods.balanceOf('0x470d3224806Db26D924F33aC79A08ED8e683e570').call()
-    this.setState({symbolvalue: result/(10 ** 15)});
-
+    this.setState({account: accounts, details: detail});
   }
 
   constructor(props) {
     super(props)
-    this.state = { account: '', details: 'test', formvalue: '', symbolvalue: ''}
-
+    this.state = {account: '', details: 'test', formvalue: '', lionbalance: ''}
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-
   }
 
   handleChange(event) {
     this.setState({formvalue: event.target.value});
   }
 
-  handleSubmit(event) {
-    //alert('A name was submitted: ' + this.state.formvalue);
-    event.preventDefault();
-
+  async getResult(){
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+    const contractAddress = '0x07b729491574F0b0b4f20c2BdDA8B306B5dC58FD'
+    var MyContract = new web3.eth.Contract(abiArray.abi, contractAddress);
+    const result = await MyContract.methods.balanceOf(this.state.formvalue).call();
+    this.setState({lionbalance: result});
+  
   }
 
-  //transfertoken(address, tokenvalue) {
-  //}
-
+  handleSubmit(event) {
+    this.getResult()
+    event.preventDefault();
+  }
+//
   render() {
     return (
       <div className="container">
         <h2>Smart Contract Simulator</h2>
         <span>My account: {this.state.account} </span>
-        <br/><span> Balance: {this.state.details}</span>
+        <br/><span> Ether-Balance: {this.state.details}</span>
         <br/><span>Form Value:  {this.state.formvalue}</span>
-        <br/><span> Symbolvalue: {this.state.symbolvalue}</span>
+        <br/><span> Liontoken-Balance: {this.state.lionbalance}</span>
       <form onSubmit={this.handleSubmit}>
         <label>
           Name:
