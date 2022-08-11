eb735bb8cb6a3174689b4f94d04ddf7474a34620
alexbertschi <alex.bertschi@gmail.com>
2022-08-11 08:57:33 +0200

    Transactions of Ether and Tokens

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import Web3 from 'web3'
 import './App.css'
 import abiArray from './Liontoken.json'
 
+let nonce = '';
+let web3 = '';
+
 class App extends Component {
+
   componentDidMount() {
     this.loadBlockchainData()
   }
 
   async loadBlockchainData() {
-    const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+    web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
     const accounts = await web3.eth.getAccounts();
     const detail = await web3.eth.getBalance(accounts[0]);
     this.setState({account: accounts, details: detail});
@@ -17,27 +21,68 @@ class App extends Component {
 
   constructor(props) {
     super(props)
-    this.state = {account: '', details: 'test', formvalue: '', lionbalance: ''}
+    this.state = {web: '', account: '', details: 'test', formvalue: '', lionbalance: '', transactionoutput: ''}
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.fireTransation = this.fireTransation.bind(this);
   }
 
   handleChange(event) {
     this.setState({formvalue: event.target.value});
   }
 
+  async fireTransation(event){
+    event.preventDefault();
+    const myAddress = '0x17E32155d034d790Fd8B88Edf949e5fEE6254C44';
+    const myPrivateKey = '596f2bd8bb26348f8d1a632779a15d189c01703808f9353e82601d2642edc936';
+    const toAddress = '0xAf415217D78fb3A13A0F1Fedc3AAdfAEfd2284B7';
+    nonce = await web3.eth.getTransactionCount(myAddress, 'latest');
+    console.log(nonce);
+    this.setState({transactionoutput: nonce});
+
+     const transaction = {
+      'to': toAddress,
+      'value': 10 ** 18,
+      'gasPrice': 30000,
+      'gasLimit': 50000,
+      'nonce': nonce,
+      // optional data field to send message or execute smart contract
+     };
+    
+     const signedTx = await web3.eth.accounts.signTransaction(transaction, myPrivateKey);
+     
+     web3.eth.sendSignedTransaction(signedTx.rawTransaction, function(error, hash) {
+     if (!error) {
+       console.log("🎉 The hash of your transaction is: ", hash);
+     } else {
+       console.log("❗Something went wrong while submitting your transaction: ", error)
+     }
+    });
+
+
+  }
+
   async getResult(){
-    const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
-    const contractAddress = '0x07b729491574F0b0b4f20c2BdDA8B306B5dC58FD'
+    //const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+    const contractAddress = '0x09A1e7b8471855Ac8d4332B3cB54315242fcC3F7'
     var MyContract = new web3.eth.Contract(abiArray.abi, contractAddress);
     const result = await MyContract.methods.balanceOf(this.state.formvalue).call();
     this.setState({lionbalance: result});
+
+     await MyContract.methods.transfer('0xAf415217D78fb3A13A0F1Fedc3AAdfAEfd2284B7', '1000000').send({from: '0x17E32155d034d790Fd8B88Edf949e5fEE6254C44'}, function(error, transactionHash){
+      if (!error) {
+        console.log("🎉 The hash of your transaction is: ", transactionHash);
+      } else {
+        console.log("❗Something went wrong while submitting your transaction: ", error)
+      }
+     }); 
+
   
   }
 
   handleSubmit(event) {
-    this.getResult()
     event.preventDefault();
+    this.getResult()
   }
 //
   render() {
@@ -48,12 +93,17 @@ class App extends Component {
         <br/><span> Ether-Balance: {this.state.details}</span>
         <br/><span>Form Value:  {this.state.formvalue}</span>
         <br/><span> Liontoken-Balance: {this.state.lionbalance}</span>
+        <br/><span> Transaction Status: {this.state.transactionoutput}</span>
       <form onSubmit={this.handleSubmit}>
         <label>
           Name:
           <input type="text" value={this.state.value} onChange={this.handleChange} />
         </label>
         <input type="submit" value="Submit" />
+      </form>
+
+      <form onSubmit={this.fireTransation}>
+        <input type="submit" value="Transaction" />
       </form>
       
       </div>
--- truffle-config.js
+++ truffle-config.js
@@ -45,7 +45,7 @@ module.exports = {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*",
-      from: '0x470d3224806Db26D924F33aC79A08ED8e683e570'       // Any network (default: none)
+      //from: '0x470d3224806Db26D924F33aC79A08ED8e683e570'       // Any network (default: none)
      },
     //
     // An additional network, but with some advanced options…
